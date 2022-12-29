a0cef4f73c2693b2a37df88a3c3e34c8e4600572
Ashley Ramsey <dusty1boy1984@gmail.com>
2022-12-29 15:49:34 -0500

    changed <a> to <Link>, corrected typo

--- src/user/PostLink.js
+++ src/user/PostLink.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { Link, useRouteMatch, useParams } from "react-router-dom"
 
 /*
   TODO: Change the below to be a link that goes to the specific post route using the post id.
 */
 
 export const PostLink = ({ post }) => {
+
+  const {url} = useRouteMatch();
+  const postId = post.id;
+
   return (
     <li className="list-group-item text-truncate">
-      <a>{post.title}</a>
+      <Link to={`${url}/${postId}`}>{post.title}</Link>
     </li>
   );
 };
